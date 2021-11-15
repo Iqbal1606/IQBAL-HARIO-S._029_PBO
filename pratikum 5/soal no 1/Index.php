517c7b0c6ed6f0d83aaac1d112f4f62b74c432c9
Iqbal1606 <72688860+Iqbal1606@users.noreply.github.com>
2021-11-16 04:09:49 +0700

    Add files via upload

---
+++ pratikum 5/soal no 1/Index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <title>Praktikum 5</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Soal 1</h2>
+
+        <!-- Program PHP -->
+        <?php
+class Pegawai
+{
+	public $nama;
+	public	function __construct($nama)
+	{
+		$this->nama = $nama;
+	}
+	public	function getNama()
+	{
+		return $this->nama;
+	}
+}
+class Manajer extends Pegawai
+{
+	public $tunjangan;
+	public	function __construct($nama, $tunjangan)
+	{
+		parent::__construct($nama);
+		$this->tunjangan = $tunjangan;
+	}
+	public	function getTunjangan()
+	{
+		return $this->tunjangan;
+	}
+}
+class Kurir extends Pegawai
+{
+	public $gaji;
+
+    public	function __construct($nama, $gaji)
+	{
+		parent::__construct($nama);
+		// $this->bonus = $bonus;
+		$this->gaji = $gaji;
+
+	}
+	public	function getGaji()
+	{
+		return $this->gaji;
+	}
+}
+class Soal1
+{
+	public static
+	function Proses($peg)
+	{
+		if ($peg instanceof Manajer)
+		{
+			$man = $peg;
+			echo "<br>Nama manajer : ".$man->nama, "\n";
+			echo "<br>Tunjangan : Rp. ".strval($man->tunjangan), "\n";
+		}
+		else if ($peg instanceof Kurir)
+		{
+			$kur =  $peg;
+			echo "<br>Nama kurir : ".$kur->nama, "\n";
+			echo "<br>Gaji : Rp. ".strval($kur->gaji), "\n";
+		}
+	}
+	public static
+	function main($args)
+	{
+		echo "INFO GAJI PEGAWAI PT IQBAL SENTOSA EXPRESS", "\n";
+		echo "<br>";
+		$peg1 = new Manajer("Iqbal", 10000000);
+		Soal1::Proses($peg1);
+		$peg2 = new Kurir("Rio", 3000000);
+		Soal1::Proses($peg2);
+	}
+}
+Soal1::main(array());
+?>
+        <!-- END PHP  -->
+    </div>
+</body>
+</html>
